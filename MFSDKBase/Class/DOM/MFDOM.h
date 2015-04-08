580ad26a0f1e74ab4bbcc26513273c834654066e
zhaoying <xcodex@126.com>
2015-04-08 12:27:26 +0800

    增加虚拟场景对象

---
+++ MFSDKBase/Class/DOM/MFDOM.h
@@ -0,0 +1,13 @@
+//
+//  MFDOM.h
+//  MFSDKBase
+//
+//  Created by 赵嬴 on 15/4/8.
+//  Copyright (c) 2015年 alipay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFDOM : NSObject
+
+@end
---
+++ MFSDKBase/Class/DOM/MFDOM.m
@@ -0,0 +1,13 @@
+//
+//  MFDOM.m
+//  MFSDKBase
+//
+//  Created by 赵嬴 on 15/4/8.
+//  Copyright (c) 2015年 alipay. All rights reserved.
+//
+
+#import "MFDOM.h"
+
+@implementation MFDOM
+
+@end
